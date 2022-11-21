e05f97ee106f07d1fdba82407f87e36a90f08988
Abdellah <abdellah.fytahi@karglobal.com>
2022-11-21 11:32:46 -0500

    Initial Commit

---
+++ inferenceclient/api/base.py
@@ -0,0 +1,3 @@
+class BaseAPI():
+    def __init__(self, ic):
+        self.ic = ic
---
+++ inferenceclient/api/datastores.py
@@ -0,0 +1,63 @@
+from .base import BaseAPI
+
+class DatastoresAPI(BaseAPI):
+    def get_datastore(self, datastore_id: str) -> dict:
+        r = self.ic._query(
+            path='https://usstudio.inferencecommunications.com/studio_instance/studio-api/v1/datastore/list-one',
+            params={
+                'datastore_id': datastore_id
+            }
+        )
+
+        return r
+
+    def get_all_datastores(self) -> dict:
+        r = self.ic._query(
+            path='https://usstudio.inferencecommunications.com/studio_instance/studio-api/v1/datastore/list-all'
+        )
+
+        return r
+
+    def get_row(self, datastore_id: str, row_id: str) -> dict:
+        r = self.ic._query(
+            path='https://usstudio.inferencecommunications.com/studio_instance/studio-api/v1/datastore/list-one-row',
+            params={
+                'datastore_id': datastore_id,
+                'data_id': row_id
+            }
+        )
+
+        return r
+
+    def get_all_rows(self, datastore_id: str) -> dict:
+        r = self.ic._query(
+            path='https://usstudio.inferencecommunications.com/studio_instance/studio-api/v1/datastore/list-all-rows',
+            params={
+                'datastore_id': datastore_id
+            }
+        )
+
+        return r
+    
+    def update_row(self, datastore_id: str, data_id: str, data: dict):
+        r = self.ic._query(
+            path='https://usstudio.inferencecommunications.com/studio_instance/studio-api/v1/datastore/update-row',
+            params={
+                'datastore_id': datastore_id,
+                'data_id': data_id,
+                'data': data
+            }
+        )
+
+        return r
+
+    def delete_row(self, datastore_id: str, data_id: str):
+        r = self.ic._query(
+            path='https://usstudio.inferencecommunications.com/studio_instance/studio-api/v1/datastore/delete-row',
+            params={
+                'datastore_id': datastore_id,
+                'data_id': data_id
+            }
+        )
+
+        return r
